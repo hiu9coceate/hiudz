b341b1a7917b8c83ce62808ee187397f892b9caf
hiu9coceate <hiubao237@gmail.com>
2025-03-24 01:27:02 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Lấy cổng từ biến môi trường PORT
+const port = process.env.PORT || 3000;  // Nếu không có `PORT`, sẽ sử dụng 3000 làm mặc định
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 document.querySelector('.btn-close').addEventListener('click', function() {
 document.querySelector('.sale').style.display='none';
 
