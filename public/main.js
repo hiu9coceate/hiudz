80affeeec81ff605605f983f9c2e3e0d4a2ee3e3
hiu9coceate <hiubao237@gmail.com>
2025-03-24 11:19:53 +0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,14 +1,25 @@
 document.querySelector('.btn-close').addEventListener('click', function() {
-document.querySelector('.sale').style.display='none';
-
-});
-
-document.querySelector('.btnSE').addEventListener('click', function(event) {
-    event.preventDefault(); 
-    const menulogo = document.querySelector('.menulogo');
-    if (menulogo.style.display === 'none' || menulogo.style.display === '') {
-        menulogo.style.display = 'flex'; 
-    } else {
-        menulogo.style.display = 'none';
-    }
-});
+    document.querySelector('.sale').style.display='none';
+    
+    });
+    
+    document.querySelector('.btnSE').addEventListener('click', function(event) {
+        event.preventDefault(); 
+        const menulogo = document.querySelector('.menulogo');
+        if (menulogo.style.display === 'none' || menulogo.style.display === '') {
+            menulogo.style.display = 'flex'; 
+        } else {
+            menulogo.style.display = 'none';
+        }
+    });
+    
+    document.querySelector('.baga').addEventListener('click', function(event) {
+        event.preventDefault();
+        const menudv = document.querySelector('.list-group');
+        if (menudv.style.display === 'none' || menudv.style.display === '') {
+            menudv.style.display = 'block'; 
+        }
+        else {
+            menudv.style.display='none';
+        }
+    });
